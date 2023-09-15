2430539973500e320997cf0da1b48186c5c64e9d
Axil <egorka.tretyakov.2017@gmail.com>
2023-09-15 22:14:14 +0300

    feat: add initial notes

--- main.js
+++ main.js
@@ -2,18 +2,44 @@ const inputElement = document.getElementById('title');
 const createBtn = document.getElementById('create');
 const listElement = document.getElementById('list');
 
+const notes = [
+    {
+        title: 'вынести мусор',
+        completed: false,
+    },
+    {
+        title: 'сходить в магазин',
+        completed: true,
+    }
+]
+
+function render() {
+    for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));
+    }
+}
+render();
+
 createBtn.onclick = () => {
     if (inputElement.value === '') {
         return;
     }
-    listElement.insertAdjacentHTML('beforeend', `
+    const newNote = {
+        title: inputElement.value,
+        completed: false,
+    }
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(newNote));
+    inputElement.value = '';
+}
+
+function getNoteTemplate(note) {
+    return `
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${inputElement.value}</span>
+            <span>${note.title}</span>
             <span>
                 <span class="btn btn-small btn-success">&check;</span>
                 <span class="btn btn-small btn-danger">&times;</span>
             </span>
         </li>
-    `);
-    inputElement.value = '';
+    `
 }
